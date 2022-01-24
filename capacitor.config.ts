1670b5aa80db968c8696cb62053f114365f02808
joycitta-siqueira <joycitta@gmail.com>
2022-01-24 14:43:38 -0300

    Initial project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tainacan-mobile',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
