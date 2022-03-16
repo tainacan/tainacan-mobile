7d57280a1235f0b4c6864010e4feac7f103b38fd
joycitta-siqueira <joycitta@gmail.com>
2022-03-15 23:27:54 -0300

    loading

--- src/pages/ItemsList.vue
+++ src/pages/ItemsList.vue
@@ -1,22 +1,36 @@
 <template>
     <base-layout page-title="Items List" page-default-back-link="/collections">
+    <ion-loading
+    :is-open="isOpenRef"
+    cssClass="my-custom-class"
+    message="Carregando..."
+    >
+    </ion-loading>
     <ion-item v-for="item of items" :key="item.id">
         <ion-label> {{ item.title }} </ion-label>
     </ion-item>
     </base-layout>
 </template>
 
-<script>
+<script lang="ts">
 import {
     IonItem,
     IonLabel,
+    IonLoading,
 } from '@ionic/vue';
 import BaseLayout from '@/components/base/BaseLayout.vue';
+import { ref } from 'vue';
 export default {
     components: {
         IonItem,
         IonLabel,
+        IonLoading,
         BaseLayout
+    },
+    setup() {
+        const isOpenRef = ref(false);
+        const setOpen = (state: boolean) => isOpenRef.value = state;
+        return { isOpenRef, setOpen }
     },
     data() {
         return {
@@ -26,10 +40,12 @@ export default {
     },
 
     created(){
+        this.setOpen(true)
         fetch(`https://museucasadahera.acervos.museus.gov.br/wp-json/tainacan/v2/collection/${this.collectionId}/items`)
             .then((response) => response.json())
             .then((data) => {
                 this.items = data.items;
+                this.setOpen(false)
             });
     },
 }
